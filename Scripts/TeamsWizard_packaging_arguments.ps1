497b8d9e0ce217d705c630f8c13e75b2857a34f2
realgarit <patrik.lleshaj@realgar.ch>
2024-10-03 22:21:22 +0200

    Update TeamsWizard_packaging_arguments.ps1

--- Scripts/TeamsWizard_packaging_arguments.ps1
+++ Scripts/TeamsWizard_packaging_arguments.ps1
@@ -1,7 +1,8 @@
 $ClientID = "61c9ddea-d334-418a-bde3-b064e4f7dac6"
 $RedirectURI = "msal61c9ddea-d334-418a-bde3-b064e4f7dac6://auth"
 $PackageType = "MSI"
-$PackageName = "Teams Wizard v0.6.7"
+$PackageName = "Teams Wizard"
+$AppVersion = "0.6.7"
 $DownloadURL = "https://github.com/realgarit/Teams-Wizard-for-Intune/raw/refs/heads/main/Apps/TeamsWizard_x64.msi"
 $TenantName = "example.onmicrosoft.com"
 $Assignment = "g_devices_testing"
@@ -15,6 +16,7 @@ Get-WmiObject -Class Win32_Product | Where {`$_.Vendor -eq 'LyncWizard.com' -and
                                         -RedirectURI $RedirectURI `
                                         -PackageType $PackageType `
                                         -PackageName $PackageName `
+                                        -AppVersion $AppVersion `
                                         -DownloadURL $DownloadURL `
                                         -TenantName $TenantName `
                                         -Assignment $Assignment `
