94f6961ff2af7ebabe9ae6c6220f77dd9de0c3c0
realgarit <patrik.lleshaj@realgar.ch>
2024-10-03 21:59:58 +0200

    Update TeamsWizard_packaging_arguments.ps1

--- Scripts/TeamsWizard_packaging_arguments.ps1
+++ Scripts/TeamsWizard_packaging_arguments.ps1
@@ -1,9 +1,9 @@
 $ClientID = "61c9ddea-d334-418a-bde3-b064e4f7dac6"
 $RedirectURI = "msal61c9ddea-d334-418a-bde3-b064e4f7dac6://auth"
-$PackageType = "EXE"
+$PackageType = "MSI"
 $PackageName = "Teams Wizard v0.6.7"
 $DownloadURL = "https://github.com/realgarit/Teams-Wizard-for-Intune/raw/refs/heads/main/Apps/TeamsWizard_x64.msi"
-$TenantName = "alzch.onmicrosoft.com"
+$TenantName = "example.onmicrosoft.com"
 $Assignment = "g_devices_testing"
 $InstallArgs = 'msiexec /i ALLUSERS=1 REBOOT=ReallySuppress /l*v "%Temp%\TeamsWizard_Install.log"'
 $UninstallArgs  = "msiexec /x /qn {8E72BB19-BE2D-4A5A-AA39-839513CF1E11}"
