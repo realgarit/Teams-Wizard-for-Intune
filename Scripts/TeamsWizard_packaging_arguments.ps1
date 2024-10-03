58eb69898e1f4b2d5ec0a307afcff120291b933a
realgarit <patrik.lleshaj@realgar.ch>
2024-10-03 23:40:56 +0200

    Update TeamsWizard_packaging_arguments.ps1

--- Scripts/TeamsWizard_packaging_arguments.ps1
+++ Scripts/TeamsWizard_packaging_arguments.ps1
@@ -6,7 +6,7 @@ $AppVersion = "0.6.7"
 $DownloadURL = "https://github.com/realgarit/Teams-Wizard-for-Intune/raw/refs/heads/main/Apps/TeamsWizard_x64.msi"
 $TenantName = "example.onmicrosoft.com"
 $Assignment = "g_devices_testing_teamswizard"
-$InstallArgs = "/i TeamsWizard_x64.msi ALLUSERS=1 REBOOT=ReallySuppress"
+$InstallArgs = "/i TeamsWizard_x64.msi ALLUSERS=1 REBOOT=ReallySuppress /qn"
 $UninstallArgs  = "msiexec /x /qn {8E72BB19-BE2D-4A5A-AA39-839513CF1E11}"
 $DetectionArgs = @"
 Get-WmiObject -Class Win32_Product | Where {`$_.Vendor -eq 'LyncWizard.com' -and `$_.Version -eq '0.6.7'}
