dc5ba0b3a5bcfff9845dbfab77de76506381f870
patriklleshaj <162427313+patriklleshaj@users.noreply.github.com>
2024-10-03 19:25:54 +0200

    Update TeamsWizard_packaging_arguments.ps1

--- Scripts/TeamsWizard_packaging_arguments.ps1
+++ Scripts/TeamsWizard_packaging_arguments.ps1
@@ -1,7 +1,9 @@
-﻿$PackageType = "EXE"
+$ClientID = "61c9ddea-d334-418a-bde3-b064e4f7dac6"
+$RedirectURI = "msal61c9ddea-d334-418a-bde3-b064e4f7dac6://auth"
+$PackageType = "EXE"
 $PackageName = "Teams Wizard v0.6.7"
-$DownloadURL = "https://github.com/patriklleshaj/Teams-Wizard-for-Intune/blob/7b4313bf017d11ac8d668973a58b44630b436544/Apps/TeamsWizard_x64.msi"
-$TenantName = "example.omicrosoft.com"
+$DownloadURL = "https://github.com/patriklleshaj/Teams-Wizard-for-Intune/raw/refs/heads/main/Apps/TeamsWizard_x64.msi"
+$TenantName = "alzch.onmicrosoft.com"
 $Assignment = "g_devices_testing"
 $InstallArgs = 'msiexec /i ALLUSERS=1 REBOOT=ReallySuppress /l*v "%Temp%\TeamsWizard_Install.log"'
 $UninstallArgs  = "msiexec /x /qn {8E72BB19-BE2D-4A5A-AA39-839513CF1E11}"
@@ -9,7 +11,9 @@ $DetectionArgs = @"
 Get-WmiObject -Class Win32_Product | Where {`$_.Vendor -eq 'LyncWizard.com' -and `$_.Version -eq '0.6.7'}
 "@
 
-.\TeamsWizard_packaging.ps1 -PackageType $PackageType `
+.\Tool_IntuneDevOpsPackaging.ps1 -ClientID $ClientID `
+                                        -RedirectURI $RedirectURI `
+                                        -PackageType $PackageType `
                                         -PackageName $PackageName `
                                         -DownloadURL $DownloadURL `
                                         -TenantName $TenantName `
