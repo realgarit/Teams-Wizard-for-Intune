4c40f75526fb62070375293025f96ebd41b40010
realgarit <patrik.lleshaj@realgar.ch>
2024-10-03 23:31:44 +0200

    Update TeamsWizard_packaging_arguments.ps1

--- Scripts/TeamsWizard_packaging_arguments.ps1
+++ Scripts/TeamsWizard_packaging_arguments.ps1
@@ -1,12 +1,12 @@
 $ClientID = "61c9ddea-d334-418a-bde3-b064e4f7dac6"
 $RedirectURI = "msal61c9ddea-d334-418a-bde3-b064e4f7dac6://auth"
-$PackageType = "EXE"
+$PackageType = "MSI"
 $PackageName = "Teams Wizard"
 $AppVersion = "0.6.7"
 $DownloadURL = "https://github.com/realgarit/Teams-Wizard-for-Intune/raw/refs/heads/main/Apps/TeamsWizard_x64.msi"
 $TenantName = "example.onmicrosoft.com"
 $Assignment = "g_devices_testing_teamswizard"
-$InstallArgs = 'msiexec /i ALLUSERS=1 REBOOT=ReallySuppress /l*v "%Temp%\TeamsWizard_Install.log"'
+$InstallArgs = "/i TeamsWizard_x64.msi ALLUSERS=1 REBOOT=ReallySuppress"
 $UninstallArgs  = "msiexec /x /qn {8E72BB19-BE2D-4A5A-AA39-839513CF1E11}"
 $DetectionArgs = @"
 Get-WmiObject -Class Win32_Product | Where {`$_.Vendor -eq 'LyncWizard.com' -and `$_.Version -eq '0.6.7'}
